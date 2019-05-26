3b40d3afe141c97cd2ab2762fd9706ca0fb67e83
Lennox-VSCode <43885619+Lennox-Elaphurus@users.noreply.github.com>
2019-05-26 23:11:21 +0800

    temp

--- main.py
+++ main.py
@@ -18,28 +18,28 @@ This just an example , not a direct input
 ['D', '-4', '-1', '-1', '-2', '-4', '7']
 """
 # read two sequence
-sequence1 ="AAAAA" #"IPGAWD"
+sequence1 = "DAADAD"#"IPGAWD"
 # sequence1=input("Please input the first sequence:")
-sequence1="0"+sequence1
+# sequence1="0"+sequence1
 print("sequence1:", sequence1)
 
 # sequence2=input("Please input the second sequence:")
-sequence2 = "AAAA"# "VGAWAD"
-sequence2="0"+sequence2
+sequence2 ="AAA"# "VGAWAD"
+# sequence2="0"+sequence2
 print("sequence2:", sequence2)
 # finish reading two sequence
 # build score matrix
 GAP = -8
-score = [[0 for i in range(len(sequence1))] for i in range(len(sequence2))]
+score = [[0 for i in range(len(sequence1)+1)] for i in range(len(sequence2)+1)]
 # use a 4-D list to represent whether two node are connected
-connectivity= [[[[False for i in range(len(sequence1))] for i in range(len(sequence2))] for i in range(len(sequence1))] for i in range(len(sequence2))]
+connectivity= [[[[False for i in range(len(sequence1)+1)] for i in range(len(sequence2)+1)] for i in range(len(sequence1)+1)] for i in range(len(sequence2)+1)]
 # initialize score matrix
-score[0][0]= 0# debugging
-for i in range(1, len(sequence2)):
+score[0][0]= 0
+for i in range(1, len(sequence2)+1):
     score[i][0]= score[i-1][0]+GAP
     connectivity[i][0][i-1][0]= True
     connectivity[i-1][0][i][0]= True
-for i in range(1, len(sequence1)):
+for i in range(1, len(sequence1)+1):
     score[0][i]= score[0][i-1]+GAP
     connectivity[0][i][0][i-1]= True
     connectivity[0][i-1][0][i]= True
@@ -49,8 +49,8 @@ def valueFromBLOSUM(i1, i2):
     global BLOSUM
     global sequence1
     global sequence2
-    charX= sequence1[i2]   # the key to find the value
-    charY= sequence2[i1]   # the key to find the value
+    charX= sequence1[i2-1]   # the key to find the value
+    charY= sequence2[i1-1]   # the key to find the value
     index1= -1   # the index of char in BLOSUM
     index2= -1   # the index of char in BLOSUM
     # find the corresponding index of char
@@ -69,32 +69,33 @@ def valueFromBLOSUM(i1, i2):
 
 for i1 in range(1, len(sequence2)):
     for i2 in range(1, len(sequence1)):
-        score[i1][i2]= max(score[i1-1][i2]+GAP, score[i1]
-                            [i2-1]+GAP, valueFromBLOSUM(i1, i2))
+        score[i1][i2]= max(score[i1-1][i2]+GAP, score[i1][i2-1]+GAP, score[i1-1][i2-1]+valueFromBLOSUM(i1, i2))
         if score[i1][i2] == score[i1-1][i2]+GAP:
             connectivity[i1][i2][i1-1][i2]= True
             connectivity[i1-1][i2][i1][i2]= True
         if score[i1][i2] == score[i1][i2-1]+GAP:
             connectivity[i1][i2][i1][i2-1]= True
             connectivity[i1][i2-1][i1][i2]= True
-        if score[i1][i2] == valueFromBLOSUM(i1, i2):
+        if score[i1][i2] == score[i1-1][i2-1]+valueFromBLOSUM(i1, i2):
             connectivity[i1][i2][i1-1][i2-1]= True
             connectivity[i1-1][i2-1][i1][i2]= True
 # print connectivity
-for i1 in range(len(sequence2)):
-    for i2 in range(len(sequence1)):
-        for i3 in range(len(sequence2)):
-            for i4 in range(len(sequence1)):
-                if connectivity[i1][i2][i3][i4]is True:
-                    print("(",i1,",",i2,"),(",i3,",",i4,")","connected.")
+# for i1 in range(len(sequence2)):
+#     for i2 in range(len(sequence1)):
+#         print("(",i1,",",i2,")score:",score[i1][i2])
+#         for i3 in range(len(sequence2)):
+#             for i4 in range(len(sequence1)):
+#                 if connectivity[i1][i2][i3][i4]is True:
+#                     print("(",i1,",",i2,"),(",i3,",",i4,")","connected.")
+
 # finish creating the score matrix
 methodCnt= 0
 
 
 def printAnswer(i1, i2, pathX, pathY):
-    print("i1:", i1, " i2:", i2)
-    print(pathX)
-    print(pathY)
+    # print("i1:", i1, " i2:", i2)
+    # print(pathX)
+    # print(pathY)
     global score
     global BLOSUM
     global sequence1
@@ -104,16 +105,16 @@ def printAnswer(i1, i2, pathX, pathY):
         for i4 in range(len(sequence1)):
             if connectivity[i1][i2][i3][i4] == True and i3 <= i1 and i4 <= i2:
                 if i3 == i1-1 and i4 == i2-1:  # come from diag
-                    pathX.insert(0, sequence1[i2])
-                    pathY.insert(0, sequence2[i1])
+                    pathX.insert(0, sequence1[i2-1])
+                    pathY.insert(0, sequence2[i1-1])
                     # printAnswer(i3, i4, pathX.copy(), pathY.copy())
                 elif i3 == i1 and i4 == i2-1:   # from left
-                    pathX.insert(0, sequence1[i2])
+                    pathX.insert(0, sequence1[i2-1])
                     pathY.insert(0, "-")
                     # printAnswer(i3, i4, pathX.copy(), pathY.copy())
                 elif i3 == i1-1 and i4 == i2:   # from up
                     pathX.insert(0, "-")
-                    pathY.insert(0, sequence2[i1])
+                    pathY.insert(0, sequence2[i1-1])
                 else:
                     print("Error: didn't find the last element.")
                 printAnswer(i3, i4, pathX.copy(), pathY.copy())
